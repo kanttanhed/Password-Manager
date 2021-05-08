ef6d790dea0124928afdc077c30e41b21e254315
kanttanhed <plinio.salazar@yahoo.com.br>
2021-05-08 09:31:47 -0300

    DB connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const app = express();
-
+const mysql = require ('mysql')
 const PORT = 3001;
+
+// Database Connection
+const db = mysql.createConnection({
+    user: 'plinio',
+    host:'localhost',
+    password: '12345678',
+    database:'PasswordManager'
+})
+
+db.connect( (err) =>{
+
+    if (err) { 
+        console.log("!!! Cannot connect !!! Error:");
+        throw err;
+    }
+    else
+    {
+       console.log("Connection established.");
+    }
+});
 
 app.get('/',(req, res) => {
     res.send("Hello Word")
